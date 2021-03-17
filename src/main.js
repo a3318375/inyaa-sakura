9c5273e165cf30ba8a245f700995be0f6d98ab33
yuxh <184608371@qq.com>
2021-03-18 03:00:29 +0800

    2

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 import {router} from './router'
 import './css/style.css'
 
+import self from "./page/self.vue"
+import contents from "./page/contents.vue"
+
 const app = createApp(App);
+app.component('self', self)
+app.component('contents', contents)
 app.use(router);
 app.mount('#app');
